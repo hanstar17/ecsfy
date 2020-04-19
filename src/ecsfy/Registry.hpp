a82104fa5a6b71d8203c605286a3fbc54a2eb899
Han-byeol Jeon <hanstar17@gmail.com>
2020-04-19 14:31:52 -0700

    cleanup

--- src/ecsfy/Registry.hpp
+++ src/ecsfy/Registry.hpp
@@ -2,40 +2,42 @@
 #include <tuple>
 #include "TypeTraits.hpp"
 namespace ecsfy {
-template <typename ...Components>
+template <typename... Components>
 struct ComponentRegistry {
   static_assert(IsUniqueSequence<Components...>::value);
 
   static constexpr int Size = sizeof...(Components);
   static constexpr int ComponentSizes[Size] = {sizeof(Components)...};
+
+  using IndexType = typename Index<Size>::Type;
   using TupleType = std::tuple<Components...>;
   using CompositionType = std::bitset<Size>;
 
   template <typename Component>
-  static constexpr int GetIndex() {
+  static constexpr IndexType GetIndex() {
     return IndexOf<Component, Components...>();
   }
 
-  template <typename ...ComponentPack>
-  static constexpr CompositionType GetComposition() {
-    if constexpr (sizeof...(ComponentPack) == 0)
-      return CompositionType();
-    else
-      return Compose<ComponentPack...>();
-  }
-  
-  template <int Id>
+  template <unsigned Index>
   static constexpr int GetSize() {
-    static_assert(Id < Size);
-    return ComponentSizes[Id];
+    static_assert(Index < Size);
+    return ComponentSizes[Index];
   }
 
-  static int GetSize(const int id) { return ComponentSizes[id]; }
+  static int GetSize(const int index) { return ComponentSizes[index]; }
+
+  template <typename... Components2>
+  static constexpr CompositionType GetComposition() {
+    if constexpr (sizeof...(Components2) == 0)
+      return CompositionType();
+    else
+      return Compose<Components2...>();
+  }
 
   TupleType values;
 
  private:
-  template<typename Component, typename ...Rest>
+  template <typename Component, typename... Rest>
   static constexpr CompositionType Compose() {
     CompositionType comp;
     comp.set(GetIndex<Component>(), true);
@@ -44,16 +46,6 @@ struct ComponentRegistry {
     else
       return comp | Compose<Rest...>();
   }
-};
-
-template <typename ...Systems>
-struct SystemRegistry {
-  static_assert(IsUniqueSequence<Systems...>::value);
-
-  static constexpr int Size = sizeof...(Systems);
-  using TupleType = std::tuple<Systems...>;
-
-  TupleType values;
 };
 
 }  // namespace ecsfy
